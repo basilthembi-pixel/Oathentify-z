cee5d2f74ffc17a29223db11691ff9b89e69a467
Basil Thembi <basilthembi@gmail.com>
2025-10-25 17:16:21 +0000

    Try fixing this error: `Runtime Error: Error: Link is not defined. Error

--- src/app/(app)/new/page.tsx
+++ src/app/(app)/new/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState, useTransition, useRef } from 'react';
+import Link from 'next/link';
 import { useForm, FormProvider, useFormContext } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
